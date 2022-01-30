136d1d552b7e2998c17b35dea39a1ea7cb0fbd8d
Mahan Fathi <mahan0fathi@gmail.com>
2022-01-29 19:49:38 -0500

    Add replay buffer sampling

--- hw1/ift6163/infrastructure/replay_buffer.py
+++ hw1/ift6163/infrastructure/replay_buffer.py
@@ -1,3 +1,4 @@
+import numpy as np
 from ift6163.infrastructure.utils import *
 
 
@@ -18,7 +19,7 @@ class ReplayBuffer(object):
         self.terminals = None
 
     def __len__(self):
-        if self.obs:
+        if self.obs is not None:
             return self.obs.shape[0]
         else:
             return 0
@@ -77,7 +78,16 @@ class ReplayBuffer(object):
         ## HINT 2: return corresponding data points from each array (i.e., not different indices from each array)
         ## HINT 3: look at the sample_recent_data function below
 
-        return TODO, TODO, TODO, TODO, TODO
+        sample_indices = np.random.choice(np.arange(len(self)), batch_size)
+        return (
+            self.obs[sample_indices],
+            self.acs[sample_indices],
+            self.rews[sample_indices],
+            self.next_obs[sample_indices],
+            self.terminals[sample_indices],
+        )
+
+        # return TODO, TODO, TODO, TODO, TODO
 
     def sample_recent_data(self, batch_size=1):
         return (
